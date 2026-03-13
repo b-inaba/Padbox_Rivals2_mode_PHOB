dbf0f4592635e6880c20adaaf6cbb2a9bf06e06a
CarVac <c.lo.to.da.down.lo@gmail.com>
2026-03-12 22:45:39 -0400

    fix phobvision overview remap

--- PhobGCC/common/phobGCC.h
+++ PhobGCC/common/phobGCC.h
@@ -26,11 +26,11 @@ using std::max;
 #include "stick.h"
 #include "../extras/extras.h"
 
-#define BUILD_RELEASE
-//#define BUILD_DEV
+//#define BUILD_RELEASE
+#define BUILD_DEV
 
 //This is just an integer.
-#define SW_VERSION 31
+#define SW_VERSION 32
 
 ControlConfig _controls{
 	.aRemap = 1 << A_REMAP,
